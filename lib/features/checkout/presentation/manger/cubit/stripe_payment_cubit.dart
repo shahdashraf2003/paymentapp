361430591cfc4692c0289d8a3847b5e933db0165
shahdashraf2003 <shahdashraf396@gmail.com>
2024-10-31 18:17:40 +0300

    create stripe payment cubit

---
+++ lib/features/checkout/presentation/manger/cubit/stripe_payment_cubit.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:payment_app/features/checkout/data/repos/checkout_repo.dart';
+
+part 'stripe_payment_state.dart';
+
+class StripePaymentCubit extends Cubit<StripePaymentState> {
+  StripePaymentCubit(this.checkoutRepo) : super(StripePaymentInitial());
+  final CheckoutRepo checkoutRepo;
+  Future makePayment({required paymentIntentInputModel}) async {
+    emit(StripePaymentLoading());
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+    data.fold(
+      (l) => emit(StripePaymentFailure(l.errMessage)),
+      (r) => emit(StripePaymentSuccess()),
+    );
+  }
+  @override
+  void onChange(Change<StripePaymentState> change) {
+    log(change.toString());
+    super.onChange(change);
+  }
+}
